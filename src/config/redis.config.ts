3009561491eabd1c0367dd1b1077a638db2d955d
krunal <139843544+krunalkadam0703@users.noreply.github.com>
2026-06-06 18:43:19 +0530

    configure redis (#4)

---
+++ src/config/redis.config.ts
@@ -0,0 +1,7 @@
+export const redisConfig = {
+  host: process.env.REDIS_HOST ?? 'localhost',
+  port: Number(process.env.REDIS_PORT ?? 6379),
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  db: Number(process.env.REDIS_DB ?? 0),
+};
